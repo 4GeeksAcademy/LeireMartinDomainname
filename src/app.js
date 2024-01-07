6ec84cb34b9f48b232c877d44f881ab3fba032c1
Leire M-Berdinos <134960908+leimber@users.noreply.github.com>
2024-01-07 06:57:02 +0000

    terminado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,47 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector(".btn").addEventListener("click", () => {
+    document.querySelector("#domain").innerHTML = generateDomainList();
+  });
 };
+
+let generarDominios = () => {
+  let pronouns = ["the", "our", "a", "that", "her", "my"];
+  let adjectives = ["fantastic", "tiny", "cute", "best", "shinny", "weird"];
+  let nouns = ["web", "corner", "net", "house", "love", "kitchen"];
+  let domains = [".com", ".net", ".es", ".org"];
+
+  let domainsList = [];
+
+  for (let i = 0; i < pronouns.length; i++) {
+    for (let j = 0; j < adjectives.length; j++) {
+      for (let k = 0; k < nouns.length; k++) {
+        for (let l = 0; l < domains.length; l++) {
+          let nombreDeDominio =
+            pronouns[i] + adjectives[j] + nouns[k] + domains[l];
+
+          domainsList.push(nombreDeDominio);
+        }
+      }
+    }
+  }
+  return domainsList;
+};
+
+generarDominios();
+
+let generateDomainList = () => {
+  let domainNames = generarDominios();
+
+  let listaDeNombres = "<ul>";
+  for (let i = 0; i < domainNames.length; i++) {
+    listaDeNombres += `<li>${domainNames[i]}</li>`;
+  }
+  listaDeNombres += "</ol>";
+
+  return listaDeNombres;
+};
+
+console.log("Hello Rigo from the console!");
